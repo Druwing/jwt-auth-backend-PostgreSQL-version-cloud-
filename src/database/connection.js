78df8a0c37a0cb35ab272aa944b28c8daaa87bdc
Druwing <143631126+Druwing@users.noreply.github.com>
2025-05-03 11:26:55 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,6 +3,7 @@ const config = require('../config');
 
 const sequelize = new Sequelize(process.env.POSTGRES_URL_CLOUD, {
   dialect: 'postgres',
+  dialectModule: require('pg'), 
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
   dialectOptions: {
     ssl: process.env.NODE_ENV === 'production' ? {
